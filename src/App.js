47b9f693ce63c0484a47511fc1202b2f5f75180c
paul_zamanian <paul.c.zamanian@gmail.com>
2022-11-29 00:11:57 -0500

    appestplusoumoinsok

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import './App.css';
 import { useRef, useState } from 'react';
 
-function openfile(file, setCode) {
-  var reader = new FileReader();
-  reader.onload = function () {
-    var text = reader.result;
-    setCode(text);
-  };
-  reader.readAsText(file);
-}
-
 function App() {
   // create variables
   var [code, setCode] = useState('!FELIPE GM - MOD7 COMPUTER ARCHITECTURE CONCORDIA CONTINUING EDUCATION\n!FINAL ASSIGNMENT FOR THE FALL CLASS 2022\n!EXAMPLE OF A SIMPLE ASSEMBLY PROGRAM USING A FEW INSTRUCTIONS. ANY LINE STARTING WITH `!` IS A COMMENT\n\n#DATA\nA 10\nB 15\nRES 0\n\n!NEXT, WE START THE CODE WITH THE #CODE MACRO\n#CODE\nLDA T0 A\nLDA T1 B\nADD T0 T1\nSTR RES T0\nHLT\n');
